5a4586bbd46c027506912056e1c059293b0e7d4f
FullStackMan <44825431+KimYoungMingoodman@users.noreply.github.com>
2019-12-01 15:27:32 +0900

    calendar
    okay

--- js/js_calendar.js
+++ js/js_calendar.js
@@ -8,74 +8,67 @@ normal_date = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 standard_day = [`일`, `월`, `화`, `수`, `목`, `금`, `토`];
 
 date = new Date();
-whatday = date.getDay();
-whatmonth = date.getMonth() - 1;
 whatyear = date.getYear() - 100;
+whatmonth = date.getMonth() - 1;
 whatdate = date.getDate();
+whatday = date.getDay();
 
-function get_firstday() {
-  tmp = (whatdate - 1) % 7;
+/* function get_firstday(year, month, date, day) {
+  tmp = (date - 1) % 7;
 
   if (whatday - tmp < 0) {
-    if (whatday === 0) firstday = 7 - tmp;
-    else firstday = 6 - tmp;
-  } else firstday = whatday - tmp;
+    if (day === 0) firstday = 7 - tmp;
+    else firstday = day - tmp;
+  } else firstday = day - tmp;
 
-  console.log(
-    `현재 년 월 일 : ${whatyear} ${whatmonth + 2} ${whatday + 1} ${
-      standard_day[whatday]
-    }`
-  );
   return firstday;
-}
+} */
 
-function getDate() {
-  firstday = get_firstday(); //1일의 요일
-
-  if (firstday === 0) normal_date[whatmonth] += +1;
+function getDate(year, month, date, day) {
+  firstday = day; //1일의 요일
 
   //고치기
-  for (i = 0, t = 0; i < normal_date[whatmonth] + t; i++) {
-    if (i <= 6) {
-      if (t < firstday) {
-        tmp1 = document.createElement("td");
-        tmp1.id = i;
-        tmp1.innerText = "";
-        row1.appendChild(tmp1);
-        t++;
-      } else {
-        tmp1 = document.createElement("td");
-        tmp1.id = i;
-        tmp1.innerText = i + 1 - t;
-        row1.appendChild(tmp1);
-      }
-    } else if (i < 14) {
+  for (t = 0; t < firstday; t++) {
+    tmp1 = document.createElement("td");
+    tmp1.id = t;
+    tmp1.innerText = "";
+    row1.appendChild(tmp1);
+  }
+
+  for (i = 0; i < normal_date[month - 1]; i++) {
+    if (i + t <= 6) {
+      tmp1 = document.createElement("td");
+      tmp1.id = i + 1;
+      tmp1.innerText = i + 1;
+      row1.appendChild(tmp1);
+    } else if (i + t < 14) {
       tmp2 = document.createElement("td");
-      tmp2.innerText = i + 1 - t;
-      tmp2.id = i;
+      tmp2.innerText = i + 1;
+      tmp2.id = i + 1;
       row2.appendChild(tmp2);
-    } else if (i < 21) {
+    } else if (i + t < 21) {
       tmp3 = document.createElement("td");
-      tmp3.innerText = i + 1 - t;
-      tmp3.id = i;
+      tmp3.innerText = i + 1;
+      tmp3.id = i + 1;
       row3.appendChild(tmp3);
-    } else if (i < 28) {
+    } else if (i + t < 28) {
       tmp4 = document.createElement("td");
-      tmp4.innerText = i + 1 - t;
-      tmp4.id = i;
+      tmp4.innerText = i + 1;
+      tmp4.id = i + 1;
       row4.appendChild(tmp4);
     } else {
       tmp5 = document.createElement("td");
-      tmp5.innerText = i + 1 - t;
-      tmp5.id = i;
+      tmp5.innerText = i + 1;
+      tmp5.id = i + 1;
       row5.appendChild(tmp5);
     }
   }
 }
 
 function init() {
-  getDate();
-  get_firstday();
+  /* getDate(whatyear, whatmonth, whatdate, whatday); */
+  /* debugger; */
+  getDate(2019, 10, 1, new Date("2019-10-01").getDay());
 }
 
 init();
