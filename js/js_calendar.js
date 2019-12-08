da38e316373bb42e8e9d7544f22d646135a18e03
FullStackMan <44825431+KimYoungMingoodman@users.noreply.github.com>
2019-12-08 11:29:45 +0900

    end

--- js/js_calendar.js
+++ js/js_calendar.js
@@ -12,12 +12,15 @@ nowmon = date.getMonth();
 nowdate = date.getDate();
 nowday = date.getDay();
 
+year_month_date = `${nowyear}-${nowmon + 1}-${nowdate}`; //오늘날짜
+
 function color_today(nowdate) {
   All_td = document.querySelectorAll("td");
 
   for (td = 0; td < All_td.length; td++) {
     if (nowdate === parseInt(All_td[td].id)) {
       All_td[td].style.backgroundColor = "#94cdfa";
+      All_td[td].style.borderRadius = `30%`;
       break;
     }
   }
@@ -101,6 +104,12 @@ function make_cal_head(year, month, day, date) {
   for (k = 0; k < 7; k++) {
     tmp0 = document.createElement("td");
 
+    if (k === 0) {
+      tmp0.style.color = `#fc6583`;
+    }
+    if (k === 6) {
+      tmp0.style.color = `#7f93ff`;
+    }
     tmp0.innerText = standard_day[k];
     t0.appendChild(tmp0);
   }
@@ -115,7 +124,6 @@ function get_calendar(year, month, day, date) {
 
   for (t = 0; t < firstday; t++) {
     tmp1 = document.createElement("td");
-
     tmp1.innerText = "";
     t1.appendChild(tmp1);
   }
@@ -144,38 +152,148 @@ function get_calendar(year, month, day, date) {
     if (i + t <= 6) {
       tmp1 = document.createElement("td");
       tmp1.id = i + 1;
-      tmp1.innerText = i + 1;
+      tmp1.innerText = i + 1; //font color red
+      if (i + t === 0) {
+        tmp1.style.color = `#fc6583`;
+      }
+      if (i + t === 6) {
+        tmp1.style.color = `#7f93ff`;
+      }
       tmp1.addEventListener("click", click_date);
+
+      year_month_date = `${nowyear}-${nowmon + 1}-${tmp1.id}`; //오늘날짜
+      if (
+        localStorage.getItem(year_month_date) &&
+        localStorage.getItem(year_month_date) !== "[]"
+      ) {
+        tmp1_div = document.createElement("div");
+        tmp1_i = document.createElement("i");
+        tmp1_div.className = "fas fa-exclamation";
+        tmp1_div.appendChild(tmp1_i);
+        tmp1.appendChild(tmp1_div);
+      }
+
       t1.appendChild(tmp1);
     } else if (i + t < 14) {
       tmp2 = document.createElement("td");
       tmp2.innerText = i + 1;
       tmp2.id = i + 1;
+      if (i + t === 7) {
+        tmp2.style.color = `#fc6583`;
+      }
+      if (i + t === 13) {
+        tmp2.style.color = `#7f93ff`;
+      }
       tmp2.addEventListener("click", click_date);
+      //
+      year_month_date = `${nowyear}-${nowmon + 1}-${tmp2.id}`; //오늘날짜
+      if (
+        localStorage.getItem(year_month_date) &&
+        localStorage.getItem(year_month_date) !== "[]"
+      ) {
+        tmp2_div = document.createElement("div");
+        tmp2_i = document.createElement("i");
+        tmp2_div.className = "fas fa-exclamation";
+        tmp2_div.appendChild(tmp2_i);
+        tmp2.appendChild(tmp2_div);
+      }
+      //
       t2.appendChild(tmp2);
     } else if (i + t < 21) {
       tmp3 = document.createElement("td");
       tmp3.innerText = i + 1;
       tmp3.id = i + 1;
+      if (i + t === 14) {
+        tmp3.style.color = `#fc6583`;
+      }
+      if (i + t === 20) {
+        tmp3.style.color = `#7f93ff`;
+      }
       tmp3.addEventListener("click", click_date);
+
+      year_month_date = `${nowyear}-${nowmon + 1}-${tmp3.id}`; //오늘날짜
+      if (
+        localStorage.getItem(year_month_date) &&
+        localStorage.getItem(year_month_date) !== "[]"
+      ) {
+        tmp3_div = document.createElement("div");
+        tmp3_i = document.createElement("i");
+        tmp3_div.className = "fas fa-exclamation";
+        tmp3_div.appendChild(tmp3_i);
+        tmp3.appendChild(tmp3_div);
+      }
+
       t3.appendChild(tmp3);
     } else if (i + t < 28) {
       tmp4 = document.createElement("td");
       tmp4.innerText = i + 1;
       tmp4.id = i + 1;
+      if (i + t === 21) {
+        tmp4.style.color = `#fc6583`;
+      }
+      if (i + t === 27) {
+        tmp4.style.color = `#7f93ff`;
+      }
       tmp4.addEventListener("click", click_date);
+
+      year_month_date = `${nowyear}-${nowmon + 1}-${tmp4.id}`; //오늘날짜
+      if (
+        localStorage.getItem(year_month_date) &&
+        localStorage.getItem(year_month_date) !== "[]"
+      ) {
+        tmp4_div = document.createElement("div");
+        tmp4_i = document.createElement("i");
+        tmp4_div.className = "fas fa-exclamation";
+        tmp4_div.appendChild(tmp4_i);
+        tmp4.appendChild(tmp4_div);
+      }
+
       t4.appendChild(tmp4);
     } else if (i + t < 35) {
       tmp5 = document.createElement("td");
       tmp5.innerText = i + 1;
       tmp5.id = i + 1;
+      if (i + t === 28) {
+        tmp5.style.color = `#fc6583`;
+      }
+      if (i + t === 34) {
+        tmp5.style.color = `#7f93ff`;
+      }
       tmp5.addEventListener("click", click_date);
+
+      year_month_date = `${nowyear}-${nowmon + 1}-${tmp5.id}`; //오늘날짜
+      if (
+        localStorage.getItem(year_month_date) &&
+        localStorage.getItem(year_month_date) !== "[]"
+      ) {
+        tmp5_div = document.createElement("div");
+        tmp5_i = document.createElement("i");
+        tmp5_div.className = "fas fa-exclamation";
+        tmp5_div.appendChild(tmp5_i);
+        tmp5.appendChild(tmp5_div);
+      }
+
       t5.appendChild(tmp5);
     } else {
       tmp6 = document.createElement("td");
       tmp6.innerText = i + 1;
       tmp6.id = i + 1;
+      if (i + t === 35) {
+        tmp6.style.color = `#fc6583`;
+      }
+
       tmp6.addEventListener("click", click_date);
+      year_month_date = `${nowyear}-${nowmon + 1}-${tmp6.id}`; //오늘날짜
+      if (
+        localStorage.getItem(year_month_date) &&
+        localStorage.getItem(year_month_date) !== "[]"
+      ) {
+        tmp6_div = document.createElement("div");
+        tmp6_i = document.createElement("i");
+        tmp6_div.className = "fas fa-exclamation";
+        tmp6_div.appendChild(tmp6_i);
+        tmp6.appendChild(tmp6_div);
+      }
       t6.appendChild(tmp6);
     }
   }
@@ -184,12 +302,13 @@ function get_calendar(year, month, day, date) {
 function click_date() {
   tmp_arr = document.querySelectorAll("td");
   for (tmp = 8; tmp < tmp_arr.length; tmp++) {
-    if (tmp_arr[tmp].style.backgroundColor !== "white") {
-      tmp_arr[tmp].style.backgroundColor = "white";
+    if (tmp_arr[tmp].style.backgroundColor !== "#7E7E7E") {
+      tmp_arr[tmp].style.backgroundColor = "#7E7E7E";
     }
   }
   target_date = event.target;
   target_date.style.backgroundColor = "#94cdfa";
+  target_date.style.borderRadius = `25%`;
 
   nowdate = target_date.id;
   day = new Date(`${nowyear}-${nowmon + 1}-${nowdate}`).getDay();
